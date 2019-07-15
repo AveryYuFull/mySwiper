d5293114c7b8a7d7e5b5d7cd3967e7d16a4a3d14
yuzhaoman <yuzhaoman@tuhu.cn>
2019-07-15 23:03:49 +0800

    pagination 的render方法

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,7 +1,77 @@
 import getElements from '../../utils/getElements';
 import addRemoveClass from '../../utils/addRemoveClass';
 import extend from '../../utils/extend';
+import callFn from '../../utils/callFn';
 const Pagination = {
+    /**
+     * 更新
+     */
+    update () {
+        const _that = this;
+        const _params = _that.params || {};
+        const _pParams = _params.pagination || {};
+        const _pagination = _that.pagination || {};
+        if (!_pParams.el || !_pagination.$el || _pagination.$el.length <= 0 ||
+            !_pagination.el) {
+            return;
+        }
+        const _snapGrid = _that.snapGrid || [];
+        let _toal = _snapGrid.length;
+        let _current = (typeof _that.snapIndex !== 'undefined' ? _that.snapIndex : _that.activeIndex) || 0;
+        const _type = (_pParams.type || '') + '';
+        switch (_type) {
+            case 'bullets':
+                if (_pParams.dynamicBullets) {
+                }
+                break;
+        }
+    },
+    /**
+     * 渲染
+     */
+    render () {
+        const _that = this;
+        const _params = _that.params || {};
+        const _pParams = _params.pagination || {};
+        const _pagination = _that.pagination || {};
+        if (!_pParams.el || !_pagination.$el || _pagination.$el.length <= 0 ||
+            !_pagination.el) {
+            return;
+        }
+        const _type = (_pParams.type || '') + '';
+        let _htmlText = '';
+        switch (_type) {
+            case 'bullets':
+                const _snapGrid = _that.snapGrid || [];
+                for (let i = 0; i < _snapGrid.length; i++) {
+                    let _html;
+                    if (_pParams.renderBullet instanceof Function) {
+                        _html = callFn(_pParams.renderBullet, [i, _pParams.bulletClass]);
+                    } else {
+                        _html = `<${_pParams.bulletElement} class="${_pParams.className}"></${_pParams.bulletElement}>`;
+                    }
+                    _htmlText += _html;
+                }
+                break;
+            case 'progressbar':
+                if (_pParams.renderProgressbar instanceof Function) {
+                    _htmlText = callFn(_pParams.renderProgressbar, [_pParams.progressbarFillClass]);
+                } else {
+                    _htmlText = `<span class="${_pParams.progressbarFillClass}"></span>`;
+                }
+                break;
+            case 'fraction':
+                if (_pParams.renderFraction instanceof Function) {
+                    _htmlText = callFn(_pParams.renderFraction, [_pParams.currentClass, _pParams.totalClass]);
+                } else {
+                    const _curHtml = `<span class="${_pParams.currentClass}"></span>`;
+                    const _totalHtml = `<span class="${_pParams.totalClass}"></span>`;
+                    _htmlText = _curHtml + '/' + _totalHtml;
+                }
+                break;
+        }
+        _pagination.el.innerHtml = _htmlText;
+    },
     /**
      * 初始化
      */
@@ -40,7 +110,7 @@ const Pagination = {
                     _classNames.push(_pParams.clickableClass);
                     _el.addEventListener('click', (evt) => {
                         const _bullets = _that.pagination.bullets || [];
-                        let _index = _bullets.find((elem) => {
+                        let _index = _bullets.findIndex((elem) => {
                             return elem === evt && evt.target;
                         });
                         _index = _index * (_params.slidesPerGroup || 1);
@@ -54,7 +124,7 @@ const Pagination = {
                 }
                 break;
         }
-        addRemoveClass(_el, _classNames);
+        addRemoveClass(_el, _classNames, true);
         extend(_that.pagination, {
             $el: _$el,
             el: _el
@@ -73,7 +143,15 @@ export default {
             clickable: true,
             clickableClass: 'swiper-pagination-clickable',
             progressbarOpposite: true,
-            progressbarOppositeClass: 'swiper-pagination-progress-opposite'
+            progressbarOppositeClass: 'swiper-pagination-progress-opposite',
+            renderBullet: null,
+            bulletClass: 'swiper-pagination-bullet',
+            bulletElement: 'span',
+            renderProgressbar: null,
+            progressbarFillClass: 'swiper-pagination-progressbar',
+            renderFraction: null,
+            currentClass: 'swiper-pagination-current',
+            totalClass: 'swiper-pagination-total'
         }
     },
     create () {
